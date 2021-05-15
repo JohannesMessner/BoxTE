Fix bug in rank calculation when time is ignored

main.py
@@ -155,14 +155,14 @@ def train_validate(kg, trainloader, valloader, model, loss_fn, optimizer, args, 
             print('first epoch done')
         if i_epoch % args.validation_step == 0:  # validation step
             print('validation checkpoint reached')
-            metrics = test(kg, valloader, model, device=device, corrupt_triples_batch_size=args.metrics_batch_size)
+            metrics = test(kg, valloader, model, args, device=device, corrupt_triples_batch_size=args.metrics_batch_size)
             print('METRICS: {}'.format(metrics))
             validation_progress.append(metrics)
             if metrics['mrr'] > best_mrr:
                 best_mrr = metrics['mrr']
                 best_params = copy.deepcopy(model.state_dict())
     print('final validation')
-    metrics = test(kg, valloader, model, device=device, corrupt_triples_batch_size=args.metrics_batch_size)
+    metrics = test(kg, valloader, model, args, device=device, corrupt_triples_batch_size=args.metrics_batch_size)
     print('METRICS: {}'.format(metrics))
     validation_progress.append(metrics)
     if metrics['mrr'] > best_mrr:
@@ -171,7 +171,7 @@ def train_validate(kg, trainloader, valloader, model, loss_fn, optimizer, args, 
     return best_params, best_mrr, {'loss': loss_progress, 'metrics': validation_progress}
 
 
-def test(kg, dataloader, model, device='cpu', corrupt_triples_batch_size=1024):
+def test(kg, dataloader, model, args, device='cpu', corrupt_triples_batch_size=1024):
     with torch.no_grad():
         batch_sizes = []
         mr = []
@@ -190,8 +190,8 @@ def test(kg, dataloader, model, device='cpu', corrupt_triples_batch_size=1024):
                 c_batch_tail, head_f_batch, tail_f_batch = tail_corrupts[i], head_f[i], tail_f[i]
                 head_c_embs = model.forward_negatives(c_batch_head)
                 tail_c_embs = model.forward_negatives(c_batch_tail)
-                ranks_head += rank(embeddings, head_c_embs, head_f_batch) - 1
-                ranks_tail += rank(embeddings, tail_c_embs, tail_f_batch) - 1
+                ranks_head += rank(embeddings, head_c_embs, head_f_batch, args.ignore_time) - 1
+                ranks_tail += rank(embeddings, tail_c_embs, tail_f_batch, args.ignore_time) - 1
             batch_sizes.append(batch.shape[2])
             mr.append(mean_rank(embeddings, ranks_head=ranks_head, ranks_tail=ranks_tail))
             mrr.append(mean_rec_rank(embeddings, ranks_head=ranks_head, ranks_tail=ranks_tail))
@@ -254,7 +254,7 @@ def train_test_val(args, device='cpu', saved_params_dir=None):
     best_params, best_mrr, progress = train_validate(kg, trainloader, valloader, model, loss_fn, optimizer, args, device=device)
     if best_params is not None:
         model = model.load_state_dict(best_params)
-    metrics = test(kg, testloader, model, device=device, corrupt_triples_batch_size=args.metrics_batch_size)
+    metrics = test(kg, testloader, model, args, device=device, corrupt_triples_batch_size=args.metrics_batch_size)
     return metrics, progress, copy.deepcopy(model.state_dict())
 
 

metrics.py
@@ -29,17 +29,17 @@ def retrieval_metrics(positive_embs, head_c_embs, tail_c_embs, filter_head, filt
     return tp, tn, fp, fn
 
 
-def rank(positive_embs, negative_embs, filter_idx):
+def rank(positive_embs, negative_embs, filter_idx, ignore_time=False):
     """
     Ranks embedded facts against provided negative fact embeddings
     :param filter_idx: indicates which of the negative embeddings are actually true and should not be considered
     """
-    scores = score(*positive_embs)
-    counterscores = score(*negative_embs)
+    scores = score(*positive_embs, ignore_time)
+    counterscores = score(*negative_embs, ignore_time)
     return ((scores > counterscores) * filter_idx).sum(dim=0) + 1  # TODO is the +1 needed to meet to def of rank?
 
 
-def mean_rank(positive_embs, head_c_embs=None, tail_c_embs=None, filter_head=None, filter_tail=None, ranks_head=None, ranks_tail=None):
+def mean_rank(positive_embs, head_c_embs=None, tail_c_embs=None, filter_head=None, filter_tail=None, ranks_head=None, ranks_tail=None, ignore_time=False):
     """
     @:param ranks_head and @:param ranks_tail can be passed to the function to avoid recalculating the rank for each metric.
         If None, ranks are calculated internally.
@@ -47,16 +47,16 @@ def mean_rank(positive_embs, head_c_embs=None, tail_c_embs=None, filter_head=Non
     if ranks_head is None:
         if head_c_embs is None or filter_head is None:
             raise ValueError("If parameter 'ranks_head' is not specified, 'head_c_embs' and 'filter_head' must be specified.")
-        ranks_head = rank(positive_embs, head_c_embs, filter_head)
+        ranks_head = rank(positive_embs, head_c_embs, filter_head, ignore_time)
     if ranks_tail is None:
         if head_c_embs is None or filter_head is None:
             raise ValueError("If parameter 'ranks_tail' is not specified, 'tail_c_embs' and 'filter_tail' must be specified.")
-        ranks_tail = rank(positive_embs, tail_c_embs, filter_tail)
+        ranks_tail = rank(positive_embs, tail_c_embs, filter_tail, ignore_time)
     batch_size = (positive_embs[0].shape)[1]
     return torch.sum(ranks_head + ranks_tail) / (2 * batch_size)
 
 
-def mean_rec_rank(positive_embs, head_c_embs=None, tail_c_embs=None, filter_head=None, filter_tail=None, ranks_head=None, ranks_tail=None):
+def mean_rec_rank(positive_embs, head_c_embs=None, tail_c_embs=None, filter_head=None, filter_tail=None, ranks_head=None, ranks_tail=None, ignore_time=False):
     """
     @:param ranks and @:param ranks_tail can be passed to the function to avoid recalculating the rank for each metric.
         If None, ranks are calculated internally.
@@ -64,18 +64,18 @@ def mean_rec_rank(positive_embs, head_c_embs=None, tail_c_embs=None, filter_head
     if ranks_head is None:
         if head_c_embs is None or filter_head is None:
             raise ValueError("If parameter 'ranks_head' is not specified, 'head_c_embs' and 'filter_head' must be specified.")
-        ranks_head = rank(positive_embs, head_c_embs, filter_head)
+        ranks_head = rank(positive_embs, head_c_embs, filter_head, ignore_time)
     if ranks_tail is None:
         if ranks_tail is None:
             if head_c_embs is None or filter_head is None:
                 raise ValueError(
                     "If parameter 'ranks_tail' is not specified, 'tail_c_embs' and 'filter_tail' must be specified.")
-        ranks_tail = rank(positive_embs, tail_c_embs, filter_tail)
+        ranks_tail = rank(positive_embs, tail_c_embs, filter_tail, ignore_time)
     batch_size = (positive_embs[0].shape)[1]
     return torch.sum(1 / ranks_head + 1 / ranks_tail) / (2 * batch_size)
 
 
-def hits_at_k(positive_embs, k, head_c_embs=None, tail_c_embs=None, filter_head=None, filter_tail=None, ranks_head=None, ranks_tail=None):
+def hits_at_k(positive_embs, k, head_c_embs=None, tail_c_embs=None, filter_head=None, filter_tail=None, ranks_head=None, ranks_tail=None, ignore_time=False):
     """
     @:param ranks and @:param ranks_tail can be passed to the function to avoid recalculating the rank for each metric.
         If None, ranks are calculated internally.
@@ -83,12 +83,12 @@ def hits_at_k(positive_embs, k, head_c_embs=None, tail_c_embs=None, filter_head=
     if ranks_head is None:
         if head_c_embs is None or filter_head is None:
             raise ValueError("If parameter 'ranks_head' is not specified, 'head_c_embs' and 'filter_head' must be specified.")
-        ranks_head = rank(positive_embs, head_c_embs, filter_head)
+        ranks_head = rank(positive_embs, head_c_embs, filter_head, ignore_time)
     if ranks_tail is None:
         if ranks_tail is None:
             if head_c_embs is None or filter_head is None:
                 raise ValueError(
                     "If parameter 'ranks_tail' is not specified, 'tail_c_embs' and 'filter_tail' must be specified.")
-        ranks_tail = rank(positive_embs, tail_c_embs, filter_tail)
+        ranks_tail = rank(positive_embs, tail_c_embs, filter_tail, ignore_time)
     batch_size = (positive_embs[0].shape)[1]
     return (torch.sum(ranks_head <= k) + torch.sum(ranks_tail <= k)) / (2 * batch_size)