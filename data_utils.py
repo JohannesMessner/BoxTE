f33c832228ede3eb3762d65caf7a06625bbe9601
Johannes Messner <messnerjo@gmail.com>
2021-05-24 10:01:34 +0100

    print faulty np arrays and exit program

--- data_utils.py
+++ data_utils.py
@@ -3,6 +3,7 @@ import logging
 import torch
 import numpy as np
 import time
+import sys
 
 
 class Temp_kg_loader():
@@ -208,7 +209,8 @@ class Temp_kg_loader():
         tuples_t = np.apply_along_axis(func, 1, tuples_t)
         if tuples_t.dtype not in ['float64', 'float32', 'float16', 'complex64', 'complex128', 'int64', 'int32', 'int16', 'int8', 'uint8', 'bool']:
             logging.warning('Array dtype not supported. No filtering in this iteration. Dtype: {}'.format(tuples_t.dtype))
-            return tuples
+            logging.info(str(tuples_t))
+            sys.exit()
         tuples_t = torch.from_numpy(tuples_t).reshape((nb_examples, batch_size, 4)).transpose(1,2).to(self.device)
         return tuples_t
 
