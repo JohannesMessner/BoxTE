f3f70d78772311633fcf19bedd2177a33bd7da73
Johannes Messner <messnerjo@gmail.com>
2021-06-12 16:10:25 +0100

    change structure of embeddings for base and time_as_boxes models

--- model.py
+++ model.py
@@ -4,6 +4,167 @@ import copy
 
 
 class BaseBoxE():
+
+    def __init__(self, embedding_dim, relation_ids, entity_ids, timestamps, weight_init='u', device='cpu',
+                 weight_init_args=(0, 1), norm_embeddings=False):
+        if weight_init == 'u':
+            self.init_f = torch.nn.init.uniform_
+        elif weight_init == 'n':
+            self.init_f = torch.nn.init.normal_
+        elif weight_init == 'default':
+            self.init_f = lambda x, *args: x  # don't change initialization
+        else:
+            raise ValueError(
+                "Invalid value for argument 'weight_init'. Use 'u' for uniform or 'n' for normal weight initialization.")
+        if norm_embeddings:
+            self.embedding_norm_fn = nn.Tanh()
+        else:
+            self.embedding_norm_fn = nn.Identity()
+        self.device = device
+        self.embedding_dim = embedding_dim
+        self.relation_ids = relation_ids
+        self.entity_ids = entity_ids
+        self.relation_id_offset = relation_ids[0]
+        self.timestamps = timestamps
+        self.max_time = max(timestamps) + 1
+        self.nb_relations = len(relation_ids)
+        self.nb_entities = len(entity_ids)
+        assert sorted(
+            entity_ids + relation_ids) == entity_ids + relation_ids, 'ids need to be ascending ints from 0 with entities coming before relations'
+
+        self.r_head_base_points = nn.Embedding(self.nb_relations, embedding_dim)
+        self.r_head_widths = nn.Embedding(self.nb_relations, embedding_dim)
+        self.r_head_size_scales = nn.Embedding(self.nb_relations, 1)
+        self.r_tail_base_points = nn.Embedding(self.nb_relations, embedding_dim)
+        self.r_tail_widths = nn.Embedding(self.nb_relations, embedding_dim)
+        self.r_tail_size_scales = nn.Embedding(self.nb_relations, 1)
+
+        self.entity_bases = nn.Embedding(self.nb_entities, embedding_dim)
+        self.entity_bumps = nn.Embedding(self.nb_entities, embedding_dim)
+
+        self.init_f(self.r_head_base_points.weight, *weight_init_args)
+        self.init_f(self.r_head_size_scales.weight, *weight_init_args)
+        self.init_f(self.r_head_widths.weight, *weight_init_args)
+        self.init_f(self.r_tail_base_points.weight, *weight_init_args)
+        self.init_f(self.r_tail_size_scales.weight, *weight_init_args)
+        self.init_f(self.r_tail_widths.weight, *weight_init_args)
+        self.init_f(self.entity_bases.weight, *weight_init_args)
+        self.init_f(self.entity_bumps.weight, *weight_init_args)
+
+    def __call__(self, positives, negatives):
+        return self.forward(positives, negatives)
+
+    def params(self):
+        return [self.r_head_base_points.weight, self.r_head_size_scales.weight, self.r_head_widths.weight,
+                self.r_tail_base_points.weight, self.r_tail_size_scales.weight, self.r_tail_widths.weight,
+                self.entity_bases.weight, self.entity_bumps.weight]
+
+    def to(self, device):
+        self.device = device
+        self.r_head_base_points = self.r_head_base_points.to(device)
+        self.r_head_size_scales = self.r_head_size_scales.to(device)
+        self.r_head_widths = self.r_head_widths.to(device)
+        self.r_tail_base_points = self.r_tail_base_points.to(device)
+        self.r_tail_size_scales = self.r_tail_size_scales.to(device)
+        self.r_tail_widths = self.r_tail_widths.to(device)
+        self.entity_bases = self.entity_bases.to(device)
+        self.entity_bumps = self.entity_bumps.to(device)
+        return self
+
+    def state_dict(self):
+        return {'r head bases': self.r_head_base_points.state_dict(), 'r head scales': self.r_head_size_scales.state_dict(),
+                'r head widths': self.r_head_widths.state_dict(),
+                'r tail bases': self.r_tail_base_points.state_dict(), 'r tail scales': self.r_tail_size_scales.state_dict(),
+                'r tail widths': self.r_tail_widths.state_dict(),
+                'entity bases': self.entity_bases.state_dict(), 'entity bumps': self.entity_bumps.state_dict()}
+
+    def load_state_dict(self, state_dict):
+        self.r_head_base_points.load_state_dict(state_dict['r head bases'])
+        self.r_head_size_scales.load_state_dict(state_dict['r head scales'])
+        self.r_head_widths.load_state_dict(state_dict['r head widths'])
+        self.r_tail_base_points.load_state_dict(state_dict['r tail bases'])
+        self.r_tail_size_scales.load_state_dict(state_dict['r tail scales'])
+        self.r_tail_widths.load_state_dict(state_dict['r tail widths'])
+        self.entity_bases.load_state_dict(state_dict['entity bases'])
+        self.entity_bumps.load_state_dict(state_dict['entity bumps'])
+        return self
+
+    def get_r_idx_by_id(self, r_ids):
+        """@:param r_names tensor of realtion ids"""
+        return r_ids - self.relation_id_offset
+
+    def get_e_idx_by_id(self, e_ids):
+        return e_ids
+
+    def compute_embeddings(self, tuples):
+        nb_examples, _, batch_size = tuples.shape
+        e_h_idx = self.get_e_idx_by_id(tuples[:, 0]).to(self.device)
+        rel_idx = self.get_r_idx_by_id(tuples[:, 1]).to(self.device)
+        e_t_idx = self.get_e_idx_by_id(tuples[:, 2]).to(self.device)
+        # get relevant embeddings
+        r_head_bases = self.r_head_base_points(rel_idx)
+        r_tail_bases = self.r_tail_base_points(rel_idx)
+        r_head_widths = nn.functional.normalize(self.r_head_widths(rel_idx), p=1, dim=2)  # normalize relative widths
+        r_tail_widths = nn.functional.normalize(self.r_tail_widths(rel_idx), p=1, dim=2)
+        r_head_scales = nn.functional.elu(self.r_head_size_scales(rel_idx)) + 1  # ensure scales > 0
+        r_tail_scales = nn.functional.elu(self.r_tail_size_scales(rel_idx)) + 1
+        # compute scaled widths
+        head_deltas = torch.multiply(r_head_widths, r_head_scales)
+        tail_deltas = torch.multiply(r_tail_widths, r_tail_scales)
+        # compute corners from base and width
+        head_corner_1 = r_head_bases + head_deltas
+        head_corner_2 = r_head_bases - head_deltas
+        tail_corner_1 = r_tail_bases + tail_deltas
+        tail_corner_2 = r_tail_bases - tail_deltas
+        # determine upper and lower corners
+        head_upper = torch.maximum(head_corner_1, head_corner_2)
+        head_lower = torch.minimum(head_corner_1, head_corner_2)
+        tail_upper = torch.maximum(tail_corner_1, tail_corner_2)
+        tail_lower = torch.minimum(tail_corner_1, tail_corner_2)
+        # assemble boxes
+        r_head_boxes = torch.stack((head_upper, head_lower), dim=2)
+        r_tail_boxes = torch.stack((tail_upper, tail_lower), dim=2)
+        # get relevant entity bases and bumps
+        head_bases = self.entity_bases(e_h_idx)
+        head_bumps = self.entity_bumps(e_h_idx)
+        tail_bases = self.entity_bases(e_t_idx)
+        tail_bumps = self.entity_bumps(e_t_idx)
+        return self.embedding_norm_fn(torch.stack((head_bases + tail_bumps, tail_bases + head_bumps), dim=2)), \
+               self.embedding_norm_fn(torch.stack((r_head_boxes, r_tail_boxes), dim=2))
+
+    def forward_negatives(self, negatives):
+        """
+        @:return tuple (entities, relations, times) containing embeddings with
+            entities.shape = (nb_negative_samples, batch_size, arity, embedding_dim)
+            relations.shape = (nb_negative_samples, batch_size, arity, 2, embedding_dim)
+            times.shape = (nb_negative_samples, batch_size, arity, 2, embedding_dim)
+        """
+        return self.compute_embeddings(negatives)
+
+    def forward_positives(self, positives):
+        return self.compute_embeddings(positives)
+
+    '''
+    @:param positives tensor containing id's for entities, relations and times of shape (1, 4, batch_size)
+        and where dim 1 indicates 0 -> head, 1 -> relation, 2 -> tail, 3 -> time
+    @:param negatives tensor containing id's for entities, relations and times of shape (nb_negative_samples, 4, batch_size)
+        and where dim 1 indicates 0 -> head, 1 -> relation, 2 -> tail, 3 -> time
+    @:return tuple ((p_entities, p_relations, p_times), (n_entities, n_relations, n_times)) with
+        p_entities.shape = (1, batch_size, arity, embedding_dim)
+        p_relations.shape = (1, batch_size, arity, 2, embedding_dim)
+        p_times.shape = (1, batch_size, arity, 2, embedding_dim)
+        n_entities.shape = (nb_negative_samples, batch_size, arity, embedding_dim)
+        n_relations.shape = (nb_negative_samples, batch_size, arity, 2, embedding_dim)
+        n_times.shape = (nb_negative_samples, batch_size, arity, 2, embedding_dim)
+    '''
+
+    def forward(self, positives, negatives):
+        positive_emb = self.forward_positives(positives)
+        negative_emb = self.forward_negatives(negatives)
+        return positive_emb, negative_emb
+
+
+class NaiveBaseBoxE():
     """
         BoxE model extended with boxes for timestamps.
         Can do interpolation completion on TKGs.
@@ -133,42 +294,93 @@ class BoxTEmp(BaseBoxE):
     """
     def __init__(self, embedding_dim, relation_ids, entity_ids, timestamps, weight_init='u', device='cpu', weight_init_args=(0, 1), norm_embeddings=False):
         super().__init__(embedding_dim, relation_ids, entity_ids, timestamps, weight_init, device, weight_init_args, norm_embeddings)
-        self.time_head_boxes = nn.Embedding(self.max_time,
-                                            2 * embedding_dim)  # lower and upper boundaries, therefore 2*embedding_dim
-        self.time_tail_boxes = nn.Embedding(self.max_time, 2 * embedding_dim)
-        self.init_f(self.time_head_boxes.weight, *weight_init_args)
-        self.init_f(self.time_tail_boxes.weight, *weight_init_args)
+        #self.time_head_boxes = nn.Embedding(self.max_time,
+                                            #2 * embedding_dim)  # lower and upper boundaries, therefore 2*embedding_dim
+        #self.time_tail_boxes = nn.Embedding(self.max_time, 2 * embedding_dim)
+        self.time_head_base_points = nn.Embedding(self.max_time, embedding_dim)
+        self.time_head_widths = nn.Embedding(self.max_time, embedding_dim)
+        self.time_head_size_scales = nn.Embedding(self.max_time, 1)
+        self.time_tail_base_points = nn.Embedding(self.max_time, embedding_dim)
+        self.time_tail_widths = nn.Embedding(self.max_time, embedding_dim)
+        self.time_tail_size_scales = nn.Embedding(self.max_time, 1)
+        self.init_f(self.time_head_base_points.weight, *weight_init_args)
+        self.init_f(self.time_head_widths.weight, *weight_init_args)
+        self.init_f(self.time_head_size_scales.weight, *weight_init_args)
+        self.init_f(self.time_tail_base_points.weight, *weight_init_args)
+        self.init_f(self.time_tail_widths.weight, *weight_init_args)
+        self.init_f(self.time_tail_size_scales.weight, *weight_init_args)
 
     def __call__(self, positives, negatives):
         return self.forward(positives, negatives)
 
     def params(self):
-        return super().params() + [self.time_head_boxes.weight, self.time_tail_boxes.weight]
+        #return super().params() + [self.time_head_boxes.weight, self.time_tail_boxes.weight]
+        return super().params() +\
+               [self.time_head_base_points.weight, self.time_head_widths.weight, self.time_head_size_scales.weight,
+                self.time_tail_base_points.weight, self.time_tail_widths.weight, self.time_tail_size_scales.weight,]
 
     def to(self, device):
         super().to(device)
-        self.time_head_boxes = self.time_head_boxes.to(device)
-        self.time_tail_boxes = self.time_tail_boxes.to(device)
+        #self.time_head_boxes = self.time_head_boxes.to(device)
+        #self.time_tail_boxes = self.time_tail_boxes.to(device)
+        self.time_head_base_points = self.time_head_base_points.to(device)
+        self.time_head_size_scales = self.time_head_size_scales.to(device)
+        self.time_head_widths = self.time_head_widths.to(device)
+        self.time_tail_base_points = self.time_tail_base_points.to(device)
+        self.time_tail_size_scales = self.time_tail_size_scales.to(device)
+        self.time_tail_widths = self.time_tail_widths.to(device)
         return self
 
     def state_dict(self):
         d = super().state_dict()
-        d['time head box'] = self.time_head_boxes.state_dict()
-        d['time tail box'] = self.time_tail_boxes.state_dict()
+        #d['time head box'] = self.time_head_boxes.state_dict()
+        #d['time tail box'] = self.time_tail_boxes.state_dict()
+        d['time head base'] = self.time_head_base_points.state_dict()
+        d['time head widths'] = self.time_head_widths.state_dict()
+        d['time head scales'] = self.time_head_size_scales.state_dict()
+        d['time tail base'] = self.time_tail_base_points.state_dict()
+        d['time tail widths'] = self.time_tail_widths.state_dict()
+        d['time tail scales'] = self.time_tail_size_scales.state_dict()
         return d
 
     def load_state_dict(self, state_dict):
         super().load_state_dict(state_dict)
-        self.time_head_boxes.load_state_dict(state_dict['time head box'])
-        self.time_tail_boxes.load_state_dict(state_dict['time tail box'])
+        #self.time_head_boxes.load_state_dict(state_dict['time head box'])
+        #self.time_tail_boxes.load_state_dict(state_dict['time tail box'])
+        self.time_head_base_points.load_state_dict(state_dict['time head base'])
+        self.time_head_widths.load_state_dict(state_dict['time head widths'])
+        self.time_head_size_scales.load_state_dict(state_dict['time head scales'])
+        self.time_tail_base_points.load_state_dict(state_dict['time tail base'])
+        self.time_tail_widths.load_state_dict(state_dict['time tail widths'])
+        self.time_tail_size_scales.load_state_dict(state_dict['time tail scales'])
         return self
 
     def compute_embeddings(self, tuples):
         entity_embs, relation_embs = super().compute_embeddings(tuples)
         nb_examples, _, batch_size = tuples.shape
         time_idx = tuples[:, 3]
-        time_head_boxes = self.time_head_boxes(time_idx).view((nb_examples, batch_size, 2, self.embedding_dim))
-        time_tail_boxes = self.time_tail_boxes(time_idx).view((nb_examples, batch_size, 2, self.embedding_dim))
+        time_head_bases = self.time_head_base_points(time_idx)
+        time_tail_bases = self.time_tail_base_points(time_idx)
+        time_head_widths = nn.functional.normalize(self.time_head_widths(time_idx), p=1, dim=2)  # normalize relative widths
+        time_tail_widths = nn.functional.normalize(self.time_tail_widths(time_idx), p=1, dim=2)
+        time_head_scales = nn.functional.elu(self.time_head_size_scales(time_idx)) + 1  # ensure scales > 0
+        time_tail_scales = nn.functional.elu(self.time_tail_size_scales(time_idx)) + 1
+        # compute scaled widths
+        head_deltas = torch.multiply(time_head_widths, time_head_scales)
+        tail_deltas = torch.multiply(time_tail_widths, time_tail_scales)
+        # compute corners from base and width
+        head_corner_1 = time_head_bases + head_deltas
+        head_corner_2 = time_head_bases - head_deltas
+        tail_corner_1 = time_tail_bases + tail_deltas
+        tail_corner_2 = time_tail_bases - tail_deltas
+        # determine upper and lower corners
+        head_upper = torch.maximum(head_corner_1, head_corner_2)
+        head_lower = torch.minimum(head_corner_1, head_corner_2)
+        tail_upper = torch.maximum(tail_corner_1, tail_corner_2)
+        tail_lower = torch.minimum(tail_corner_1, tail_corner_2)
+        # assemble boxes
+        time_head_boxes = torch.stack((head_upper, head_lower), dim=2)
+        time_tail_boxes = torch.stack((tail_upper, tail_lower), dim=2)
         return entity_embs, relation_embs, self.embedding_norm_fn(torch.stack((time_head_boxes, time_tail_boxes), dim=2))
 
 
